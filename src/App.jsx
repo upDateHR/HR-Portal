91e37c58987b08012a0b8b88af31638d13e99b0e
Raushankumar0001 <rksoni7764@gmail.com>
2025-11-14 18:57:52 +0530

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
  
 
   return (
     <>
+      <Navbar/>
       <h1 className='text-black font-bold text-9xl'>Hey Guys</h1>
     </>
   )
